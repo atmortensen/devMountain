1f2ca53a7fef2bfb33ae06b0a1099901e31c8f8d
Alexander Mortensen <alextmortensen@gmail.com>
2017-03-09 10:46:43 -0700

    Not done yet

---
+++ mini-userProfiles3-master/service.js
@@ -0,0 +1,16 @@
+angular.module('userProfiles').service('mainService', function($http, $q) {
+
+  this.getUsers = function() {
+  	var deferred = $q.defer();
+    $http({
+        method: 'GET',
+        url: 'https://reqres.in/api/users?page=1'
+    }).then(function(response){
+    	var parsedResponse = response.data.data;
+    	deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
+
+
+});
