9eb087f9724a9be3976f50d03fb5dbc58787a190
Alexand Mortensen <alextmortensen@gmail.com>
2017-03-21 14:47:18 -0600

    chatty-master

---
+++ chatty-master/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+
+var messages = []
+app.get('/messages', function(req, res, next){
+	res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({message: req.body.message, time: moment().format('h:mm a') });
+  res.status(200).json({ messages: messages });
+});
+
+
+app.listen(3000, function(){
+	console.log('Server Running');
+});
+
+
